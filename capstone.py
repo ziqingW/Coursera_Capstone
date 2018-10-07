17c2e8b1d7ea3012270499c4aaa4f7b0c646d49e
Linda Lee <redtaq@hotmail.com>
2018-10-07 12:52:01 -0500

    setup

---
+++ capstone.py
@@ -0,0 +1,9 @@
+from IPython.display import Markdown
+
+Markdown("""# Introduction
+### This is the capstone project for Data Science Course certified by IBM
+""")
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
